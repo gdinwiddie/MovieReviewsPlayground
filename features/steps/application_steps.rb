f11e819ed4938ac0f42f7b1f68924704d820715f
gdinwiddie <gdinwiddie@wildebeest.home>
2010-11-23 10:55:37 -0500

    added scenarios 2 & 3 (pending)

--- features/steps/application_steps.rb
+++ features/steps/application_steps.rb
@@ -1,7 +1,15 @@
+Given /^I load (\d+) movie[s]? in the movie list$/ do |movie_count|
+  pending # express the regexp above with the code you wish you had
+end
+
 When /^I visit the homepage$/ do
   visit('/')
 end
 
 Then /^I see a list of movies$/ do
   page.should have_xpath('//table')
+end
+
+Then /^I see a list with (\d+) movie[s]?$/ do |movie_count|
+  pending # express the regexp above with the code you wish you had
 end
