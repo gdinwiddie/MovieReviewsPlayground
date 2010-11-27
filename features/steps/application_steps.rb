86d54227d413e459f71f56f966178008e7beb4f6
Jon Kern <jonkern@comcast.net>
2010-11-26 23:10:09 -0500

    added Movie class stub

--- features/steps/application_steps.rb
+++ features/steps/application_steps.rb
@@ -1,5 +1,20 @@
+class Movie
+  attr_accessor :title
+  
+  def initialize( title )
+    @title = title
+  end
+  
+  def to_s
+    @title
+  end
+end
+
 Given /^I load (\d+) movie[s]? in the movie list$/ do |movie_count|
-  pending # express the regexp above with the code you wish you had
+  (1..movie_count.to_i).each do |i|
+    @movie = Movie.new( "#{i} Movie" )
+  end
+  
 end
 
 When /^I visit the homepage$/ do
