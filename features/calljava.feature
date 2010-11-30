ccd4fc078988daf23ae37dbf26ff33d28a024757
gdinwiddie <gdinwiddie@wildebeest.home>
2010-11-30 14:41:00 -0500

    added a step written in Java

---
+++ features/calljava.feature
@@ -0,0 +1,4 @@
+Feature: Using Steps written in Java
+
+Scenario:
+  Then I should successfully call Java
---
+++ javasteps/steps/Javasteps.java
@@ -0,0 +1,10 @@
+package steps;
+
+import cuke4duke.annotation.I18n.EN.Then;
+
+public class Javasteps {
+	@Then ("^I should successfully call Java$")
+	public void iShouldSuccessfullyCallJava() {
+	}
+	
+}
